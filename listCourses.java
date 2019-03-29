67cd1671c45218f33f2e8627cc6c05fbb5627633
Assalat3 <34478752+Assalat3@users.noreply.github.com>
2019-03-29 17:29:54 +0200

    Update listCourses.java

--- listCourses.java
+++ listCourses.java
@@ -9,8 +9,8 @@ public class listCourses {
 
         //DB info
         final String dburl = "jdbc:mysql://localhost:3306/opsuor";
-        final String dbusername = "root";
-        final String dbpass = "W0tM8J0p";
+        final String dbusername = "USERNAME";
+        final String dbpass = "PASSWORD";
 
         try{
             //Yhdistetään tietokantaan
